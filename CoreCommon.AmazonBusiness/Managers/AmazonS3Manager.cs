ae2210da01542d5c2f1ba55c35a00754acad1d1b
Enes Aydın <enaydin@protel.com.tr>
2022-09-25 21:19:02 +0300

    Add GetObjectStreamByKey On AmazonS3Manager

--- CoreCommon.AmazonBusiness/Managers/AmazonS3Manager.cs
+++ CoreCommon.AmazonBusiness/Managers/AmazonS3Manager.cs
@@ -3,6 +3,7 @@ using Amazon.S3;
 using Amazon.S3.Transfer;
 using CoreCommon.AmazonBusiness.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Linq;
@@ -55,7 +56,13 @@ namespace CoreCommon.AmazonBusiness.helpers
                 Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
                 throw e;
             }
+
             return false;
+        }
+
+        public Task<Stream> GetObjectStreamByKey(string bucketName, string keyName)
+        {
+            return s3Client.GetObjectStreamAsync(bucketName, keyName, new Dictionary<string, object>());
         }
     }
 }
