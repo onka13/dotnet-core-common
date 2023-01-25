e394aba9e6fd3b3c45df3a7739d9fd37d1b0adf3
onka13 <onurkaya13@live.com>
2023-01-25 00:56:39 +0000

    add test methods

--- CoreCommon.Application.WebAPIBase/Controllers/CommonDebugController.cs
+++ CoreCommon.Application.WebAPIBase/Controllers/CommonDebugController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreCommon.Infrastructure.Helpers;
+using CoreCommon.Infrastructure.Logging;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
 
 namespace CoreCommon.Application.WebAPIBase.Controllers
 {
@@ -8,6 +13,8 @@ namespace CoreCommon.Application.WebAPIBase.Controllers
     public class CommonDebugController : BaseController
     {
         public IConfiguration Configuration { get; set; }
+
+        public ILogService LogService { get; set; }
 
         [HttpGet("info")]
         public IActionResult Info()
@@ -19,6 +26,39 @@ namespace CoreCommon.Application.WebAPIBase.Controllers
                 Version = Configuration["Version"],
                 Environment = Configuration["ASPNETCORE_ENVIRONMENT"],
             });
+        }
+
+        [HttpGet("logtest")]
+        public IActionResult LogTest()
+        {
+            foreach (var loglevel in Enum.GetValues<LogLevel>())
+            {
+                LogService.Log(loglevel, $"This is a {loglevel} message {DateTime.UtcNow}");
+            }
+
+            return SuccessResponse("Done");
+        }
+
+        [HttpGet("test/get")]
+        public async Task<IActionResult> TestGet()
+        {
+            return await TestResponse();
+        }
+
+        [HttpPost("test/post")]
+        public async Task<IActionResult> TestPost()
+        {
+            return await TestResponse();
+        }
+
+        private async Task<IActionResult> TestResponse()
+        {
+            if (Request.Query.ContainsKey("error"))
+            {
+                return ErrorResponse(message: Request.Query["error"]);
+            }
+
+            return SuccessResponse(await StreamHelper.ToStringAsync(Request.Body));
         }
     }
 }
